1d45353e4b6871b20bd4f50eed9932b6fc08178c
samyadeepbasu <f2013401@pilani.bits-pilani.ac.in>
2016-06-08 18:09:46 +0200

    more fixes

--- d3_index.php
+++ d3_index.php
@@ -127,7 +127,7 @@ $jsonarray = json_encode($return);
 
 //Writing the JSON object into a .json file for d3.js to access it
 
-$fh= fopen("mis.json",'w') or die("Error opening file");
+$fh= fopen("miss.json",'w') or die("Error opening file");
 
 fwrite($fh,$jsonarray);
 
@@ -146,6 +146,9 @@ curl_close($curl);
 ?>
 
 
+
+
+
 <!DOCTYPE html>
 <html>
 <meta charset="utf-8">
@@ -160,21 +163,22 @@ curl_close($curl);
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.3/jquery.min.js"></script>
-
+<!-- Optional theme -->
+<!-- Optional theme -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
 
 
-
+<!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
-
+<!--  <link rel="stylesheet" href="css/style.css" /> -->
 
 <link rel="stylesheet" href="tablecss.CSS" />
 
 <style>
 
 body {
-    padding-top: 70px;
+    padding-top: 70px; /* Required padding for .navbar-fixed-top. Change if height of navigation changes. */
 
 }
 
@@ -292,7 +296,7 @@ color:#000;
 
    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
-            
+            <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                     <span class="sr-only">Toggle navigation</span>
@@ -304,7 +308,7 @@ color:#000;
                     <!-- <img src="tripal.png"  alt=""> -->
                 </a>
             </div>
-           
+            <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
                     <li>
@@ -352,7 +356,7 @@ var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height).attr("id","svg-id").append("g").call(d3.behavior.zoom().scaleExtent([1,8]).on("zoom",zoom)).append("g");
 
-d3.json("mis.json", function(error, graph) {
+d3.json("miss.json", function(error, graph) {
   if (error) throw error;
 
   force
@@ -425,15 +429,18 @@ function zoom() {
         <div id="collapseFive" class="panel-collapse collapse">
             <div class="panel-body">
 
-               <form method="post" action="retrieve.php" id="species">
+               <form method="post" action="d3_index.php" id="species">
                  <select id="module" name="module">
-                    <option>Rice</option>
-                    <option>Arabidopsis</option>
-                    <option>Orza Sativa</option>
+                    <option>Module1</option>
+                    <option>Module2</option>
+                    <option>Module3</option>
+                    <option>Module4</option>
+                    <option>Module5</option>
+                    <option>Module6</option>
                 
                  </select>
                 
-                 <input type="submit" />
+                 <input type="submit" name="submit" />
 
               </form>
               
