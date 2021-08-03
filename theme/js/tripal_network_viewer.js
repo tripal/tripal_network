6816bd2e3ffa22a07e157da2d62a566bb15e1ca7
Stephen Ficklin <spficklin@gmail.com>
2021-08-03 16:16:32 -0700

    GraphML loader working.  Now working on visualization

--- theme/js/tripal_network_viewer.js
+++ theme/js/tripal_network_viewer.js
@@ -247,16 +247,14 @@
     // The data needed to retreive the network is provided to this function
     // by the tripal_network module of Drupal via the data function. 
     var network_id = filters['network_id'];
-    var module_id = filters['module_id'];
-    var trait_id = filters['trait_id'];
-    var node_id = filters['node_id'];
+    var feature_id = filters['feature_id'];
     $.ajax({
       // The baseurl is a variable set by Tripal that indicates the
       // "base" of the URL for this site.
       url: baseurl + '/networks/retrieve',
       type: "GET",
       dataType: 'json',
-      data: {'network_id': network_id, 'module_id': module_id, 'trait_id': trait_id, 'node_id': node_id },
+      data: {'network_id': network_id, 'feature_id': feature_id },
       success: function(json) {
         Network_Data = json;
         loadNetwork(Network_Data);
